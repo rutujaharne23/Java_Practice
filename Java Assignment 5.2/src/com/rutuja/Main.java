ae5774829cf9878564e4ab4baf4018fedf354867
Rutuja Harne <80842769+rutujaharne23@users.noreply.github.com>
2021-12-26 00:46:43 +0530

    Java practice - tenth commit

---
+++ Java Assignment 5.2/src/com/rutuja/Main.java
@@ -0,0 +1,38 @@
+package com.rutuja;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        // Fine the floor of a number
+        // floor -> greater number smaller or equal to target
+        5Scanner in = new Scanner(System.in);
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+
+        System.out.println("Enter target: ");
+        int target = in.nextInt();
+
+        int ans = searchNumber(arr, target);
+        System.out.println(ans);
+    }
+
+    private static int searchNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }else if(target > arr[mid]){
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
