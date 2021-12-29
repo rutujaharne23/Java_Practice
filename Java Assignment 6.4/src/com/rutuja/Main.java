9746d638a723781ea2d8f91116463bff4cda71cc
Rutuja Harne <80842769+rutujaharne23@users.noreply.github.com>
2021-12-29 15:58:07 +0530

    Java practice - fifteen commit

---
+++ Java Assignment 6.4/src/com/rutuja/Main.java
@@ -0,0 +1,32 @@
+package com.rutuja;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        // Cycle Sort
+        int[] arr = {3, 5, 2, 1, 4};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycle(int[] arr) {
+        int i = 0;
+        while (i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }else { 
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
